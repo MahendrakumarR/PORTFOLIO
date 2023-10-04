5aeff245814812e504a8ee66b5bad4858d511d5b
MahendraN <mahendraramalingam8056@gmail.com>
2023-10-04 13:47:05 +0530

    updated

--- resume/urls.py
+++ resume/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns=[
-    path("", views.new, name="new")
+    path("", views.new, name="new"),
+    path("msc/", views.msc, name="msc"),
+    path("bsc/", views.bsc, name="bsc") 
 ]
--- resume/views.py
+++ resume/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def new(request):
     return render(request,"resume/new.html")
+
+def msc(request):
+    return render(request,"resume/msc.html")
+
+def bsc(request):
+    return render(request,"resume/bsc.html")
